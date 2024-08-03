330c977d40973c1ada6bb29e626a4d2bc918faeb
oliveira-86-c1risk <egberto.duarte.14@gmail.com>
2024-08-03 16:10:10 -0300

    tanstak query setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,29 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
+import { ActivityIndicator, StyleSheet, FlatList } from 'react-native';
 import { Text, View } from '@/components/Themed';
-import { useEffect } from 'react';
 import { fetchTopRatedMovies } from '@/api/movies';
+import { useQuery } from '@tanstack/react-query';
 
 export default function TabOneScreen() {
-  useEffect(() => {
-    fetchTopRatedMovies()
-  }, [])
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['movies'],
+    queryFn: fetchTopRatedMovies,
+  });
+
+  console.log("data: ", data);
+
+  if (isLoading) return <ActivityIndicator />
+
+  if (error) return <Text>{error.message}</Text>
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <FlatList 
+        data={data.results}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Text>{item.original_title}</Text>}
+      />
     </View>
   );
 }
